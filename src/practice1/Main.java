dfa65a5a0aaf3d1d6769aab66fc0f9575b01ce6a
Sofiya18 <56328619+Sofiya18@users.noreply.github.com>
2019-11-10 11:29:03 -0600

    something interesting

--- src/practice1/Main.java
+++ src/practice1/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code her
         System.out.println("perfect");
+
+
+        System.out.println("Everything will be perfect");
     }
 }
